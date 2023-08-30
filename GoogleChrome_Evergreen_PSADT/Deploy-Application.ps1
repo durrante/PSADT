adcc16d391b89c02c70a84f0a300f218d1cd602c
durrante <Alexdurrant_16@hotmail.com>
2023-08-30 11:12:12 +0100

    Update Deploy-Application.ps1
    Adds logic to remove desktop shortcuts.

--- GoogleChrome_Evergreen_PSADT/Deploy-Application.ps1
+++ GoogleChrome_Evergreen_PSADT/Deploy-Application.ps1
@@ -182,6 +182,25 @@ Try {
 		Remove-Folder -Path "$env:SYSTEMDRIVE\temp" -ErrorAction SilentlyContinue 
 		Remove-File -Path "$envCommonDesktop\Google Chrome.lnk" -ErrorAction SilentlyContinue
 
+		## Prevent creation of the Desktop shortcut
+		$InstallLocation = Split-Path ((Get-ItemProperty -Path "HKLM:\SOFTWARE\Classes\ChromeHTML\shell\open\command" -Name "(Default)")."(Default)").Split("--")[0].Trim('"',' ') -Parent
+		$PrefFile = "$($InstallLocation)\initial_preferences"
+		if (Test-Path -Path "$PrefFile" -PathType Leaf) {
+			try {
+				$json = Get-Content $PrefFile | ConvertFrom-Json
+				If ($json) {
+					If ($json.distribution -ne $null) {
+						$json.distribution | Add-Member -Name "do_not_create_desktop_shortcut" -Value $true -MemberType NoteProperty -Force
+					}
+					Else {
+						$json | Add-Member -Name "distribution" -Value @{"do_not_create_desktop_shortcut"=$true} -MemberType NoteProperty -Force
+					}
+					$json | ConvertTo-Json -Depth 100 | Out-File -FilePath $PrefFile -Encoding ASCII
+				}
+			}
+			catch {}
+		}
+		
 		## Display a message at the end of the install
 		## If (-not $useDefaultMsi) { Show-InstallationPrompt -Message 'You can customize text to appear at the end of an install or remove it completely for unattended installations.' -ButtonRightText 'OK' -Icon Information -NoWait }
 	}
