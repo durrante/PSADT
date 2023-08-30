4603132ceef34dde59ec6edfddd28a4a65436a6d
durrante <Alexdurrant_16@hotmail.com>
2023-08-30 10:03:02 +0100

    Update Citrix Workspace App
    Update Citrix Workspace App and Detection Method.

--- CitrixWorkspaceApp_Evergreen_PSADT/Deploy-Application.ps1
+++ CitrixWorkspaceApp_Evergreen_PSADT/Deploy-Application.ps1
@@ -67,8 +67,8 @@ Try {
 	[string]$appArch = 'x86'
 	[string]$appLang = 'EN'
 	[string]$appRevision = '01'
-	[string]$appScriptVersion = '1.0.0'
-	[string]$appScriptDate = '30/05/2022'
+	[string]$appScriptVersion = '1.1.0'
+	[string]$appScriptDate = '30/08/2023'
 	[string]$appScriptAuthor = 'Alex Durrant'
 	##*===============================================
 	## Variables: Install Titles (Only set here to override defaults set by the toolkit)
@@ -153,8 +153,39 @@ Try {
 		}
 
 		## <Perform Installation tasks here>
-		[string]$MyArguments = "-ExecutionPolicy Bypass -NoLogo -File `"$dirFiles\InstallCitrixWorkspace.ps1`" "
-		Execute-Process -Path "$PSHOME\powershell.exe" -Arguments $MyArguments
+		# Trust PowerShell Gallery
+		If ((Get-PSRepository | Where-Object { $_.Name -eq "PSGallery" -and $_.InstallationPolicy -ne "Trusted" })) {
+		    # Install NuGet package provider, which is required to trust the PowerShell Gallery
+		    Install-PackageProvider -Name "NuGet" -MinimumVersion 2.8.5.208 -Force
+		    # Trust the PowerShell Gallery
+		    Set-PSRepository -Name "PSGallery" -InstallationPolicy "Trusted"
+		}
+		
+		# Install or update Evergreen module
+		$InstalledEvergreen = Get-Module -Name "Evergreen" -ListAvailable | Sort-Object -Property Version -Descending | Select-Object -First 1
+		$PublishedEvergreen = Find-Module -Name "Evergreen"
+		
+		If ($null -eq $InstalledEvergreen) {
+		    # Evergreen module is not installed, so install it
+		    Install-Module -Name "Evergreen"
+		}
+		ElseIf ($PublishedEvergreen.Version -gt $InstalledEvergreen.Version) {
+		    # A newer version of the Evergreen module is available, so update it
+		    Update-Module -Name "Evergreen"
+		}
+		
+		# Application-specific variables
+		$appName = "CitrixWorkspaceApp"
+		$appTitle = "Citrix Workspace - Current Release"
+		$tempPath = "C:\Temp\$appName"
+		
+		# Download the latest stable version of the application using the Evergreen module
+		$appInfo = Get-EvergreenApp -Name $appName | Where-Object { $_.Title -eq $appTitle}
+		$installerPath = $appInfo | Save-EvergreenApp -Path $tempPath
+
+		# Install Citrix
+		Start-Process -FilePath "$InstallerPath" -args "/noreboot /silent EnableCEIP=false" -Verbose
+		Sleep -Seconds 60
 		
 		##*===============================================
 		##* POST-INSTALLATION
