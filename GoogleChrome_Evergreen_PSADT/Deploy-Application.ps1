478e680b1be8fdd5f5bf8cdb5a9c9983d2f1bd4c
durrante <64601521+durrante@users.noreply.github.com>
2023-08-30 09:26:40 +0100

    Update Deploy-Application.ps1

--- GoogleChrome_Evergreen_PSADT/Deploy-Application.ps1
+++ GoogleChrome_Evergreen_PSADT/Deploy-Application.ps1
@@ -137,8 +137,40 @@ Try {
 		}
 
 		## <Perform Installation tasks here>
-		[string]$MyArguments = "-ExecutionPolicy Bypass -NoLogo -File `"$dirFiles\InstallGoogleChrome.ps1`" "
-		Execute-Process -Path "$PSHOME\powershell.exe" -Arguments $MyArguments
+		# Trust PowerShell Gallery
+		If ((Get-PSRepository | Where-Object { $_.Name -eq "PSGallery" -and $_.InstallationPolicy -ne "Trusted" })) {
+		    # Install NuGet package provider, which is required to trust the PowerShell Gallery
+		    Install-PackageProvider -Name "NuGet" -MinimumVersion 2.8.5.208 -Force
+		    # Trust the PowerShell Gallery
+		    Set-PSRepository -Name "PSGallery" -InstallationPolicy "Trusted"
+		}
+		
+		# Install or update Evergreen module
+		$InstalledEvergreen = Get-Module -Name "Evergreen" -ListAvailable | Sort-Object -Property Version -Descending | Select-Object -First 1
+		$PublishedEvergreen = Find-Module -Name "Evergreen"
+		
+		If ($null -eq $InstalledEvergreen) {
+		    # Evergreen module is not installed, so install it
+		    Install-Module -Name "Evergreen"
+		}
+		ElseIf ($PublishedEvergreen.Version -gt $InstalledEvergreen.Version) {
+		    # A newer version of the Evergreen module is available, so update it
+		    Update-Module -Name "Evergreen"
+		}
+		
+		# Application-specific variables
+		$appName = "GoogleChrome"
+		$appArchitecture = "x64"
+		$appChannel = "Stable"
+		$appType = "msi"
+		$tempPath = "C:\Temp\$appName"
+		
+		# Download the latest stable version of the application using the Evergreen module
+		$appInfo = Get-EvergreenApp -Name $appName | Where-Object { $_.Architecture -eq $appArchitecture -and $_.Channel -eq $appChannel -and $_.Type -eq $appType }
+		$installerPath = $appInfo | Save-EvergreenApp -Path $tempPath
+		
+		# Install the application
+		Start-Process -FilePath msiexec.exe -ArgumentList "/I `"$installerPath`" /quiet" -Wait -Verbose
 		
 		##*===============================================
 		##* POST-INSTALLATION
