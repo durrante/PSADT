005390d6dc62237254471e8a7e4029a7fd8bdfdf
durrante <Alexdurrant_16@hotmail.com>
2025-11-13 16:28:42 +0000

    Updating 7-Zip

--- GoogleChrome_Evergreen_PSADT/Deploy-Application.ps1
+++ GoogleChrome_Evergreen_PSADT/Deploy-Application.ps1
@@ -138,25 +138,25 @@ Try {
 
 		## <Perform Installation tasks here>
 		# Trust PowerShell Gallery
-		If ((Get-PSRepository | Where-Object { $_.Name -eq "PSGallery" -and $_.InstallationPolicy -ne "Trusted" })) {
-		    # Install NuGet package provider, which is required to trust the PowerShell Gallery
-		    Install-PackageProvider -Name "NuGet" -MinimumVersion 2.8.5.208 -Force
-		    # Trust the PowerShell Gallery
-		    Set-PSRepository -Name "PSGallery" -InstallationPolicy "Trusted"
+		if (Get-PSRepository | Where-Object { $_.Name -eq "PSGallery" -and $_.InstallationPolicy -ne "Trusted" }) {
+			Install-PackageProvider -Name "NuGet" -MinimumVersion 2.8.5.208 -Force
+			Set-PSRepository -Name "PSGallery" -InstallationPolicy "Trusted"
+		}
+
+		#Install or update Evergreen module
+		$Installed = Get-Module -Name "Evergreen" -ListAvailable | `
+			Sort-Object -Property @{ Expression = { [System.Version]$_.Version }; Descending = $true } | `
+			Select-Object -First 1
+		$Published = Find-Module -Name "Evergreen"
+		if ($Null -eq $Installed) {
+			Install-Module -Name "Evergreen"
+		}
+		elseif ([System.Version]$Published.Version -gt [System.Version]$Installed.Version) {
+			Update-Module -Name "Evergreen"
 		}
 		
-		# Install or update Evergreen module
-		$InstalledEvergreen = Get-Module -Name "Evergreen" -ListAvailable | Sort-Object -Property Version -Descending | Select-Object -First 1
-		$PublishedEvergreen = Find-Module -Name "Evergreen"
-		
-		If ($null -eq $InstalledEvergreen) {
-		    # Evergreen module is not installed, so install it
-		    Install-Module -Name "Evergreen"
-		}
-		ElseIf ($PublishedEvergreen.Version -gt $InstalledEvergreen.Version) {
-		    # A newer version of the Evergreen module is available, so update it
-		    Update-Module -Name "Evergreen"
-		}
+		## Download latest manifest
+		Update-Evergreen -force
 		
 		# Application-specific variables
 		$appName = "GoogleChrome"
